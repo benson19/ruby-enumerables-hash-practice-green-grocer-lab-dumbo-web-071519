f173ba56f2e073bfdabd049707b49d446e1a7bc5
Matthew Guzman <tomaffew@me.com>
2019-07-09 05:58:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -12,7 +12,17 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+   if coupons.length!=0
+   for i in coupons
+     if cart.include?(i[:item]) && cart[i[:item]][:count] >= i[:num]
+       cart[i[:item] + " W/COUPON"] = {price: i[:cost]/i[:num]}
+       cart[i[:item] + " W/COUPON"][:clearance] = cart[i[:item]][:clearance]
+       cart[i[:item] + " W/COUPON"][:count] = cart[i[:item]][:count] / i[:num] * i[:num]
+       cart[i[:item]][:count] -= cart[i[:item] + " W/COUPON"][:count]
+     end
+   end
+ end
+ cart
 end
 
 def apply_clearance(cart)
